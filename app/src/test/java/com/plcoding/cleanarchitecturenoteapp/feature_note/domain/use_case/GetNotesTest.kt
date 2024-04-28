624e5bce4714de102659eaca648a45588e8780f5
yoshi1075 <agy6786@gmail.com>
2024-04-28 16:47:59 +0900

    Finished App

---
+++ app/src/test/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/use_case/GetNotesTest.kt
@@ -0,0 +1,44 @@
+package com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case
+
+import com.plcoding.cleanarchitecturenoteapp.feature_note.data.repository.FakeNoteRepository
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.Note
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.util.NoteOrder
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.util.OrderType
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+
+class GetNotesTest {
+    private lateinit var getNotes: GetNotes
+
+    private lateinit var fakeNoteRepository: FakeNoteRepository
+
+    @Before
+    fun setUp() {
+        fakeNoteRepository = FakeNoteRepository()
+        getNotes = GetNotes(fakeNoteRepository)
+
+        val notes = ('a'..'z').mapIndexed { index, c ->
+            Note(
+                c.toString(),
+                c.toString(),
+                index.toLong(),
+                index,
+            )
+        }.shuffled()
+
+        runBlocking {
+            notes.forEach { fakeNoteRepository.insertNote(it) }
+        }
+    }
+
+    @Test
+    fun `Order notes by title ascending, correct order`() = runBlocking {
+        val notes = getNotes(NoteOrder.Title(OrderType.Ascending)).first()
+        for (i in 0..(notes.size - 2)) {
+            assertThat(notes[i].title).isLessThan(notes[i+1].title)
+        }
+    }
+}
